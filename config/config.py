605f25f84b25f6e4ce30a5ac300c2b70141a04f4
stepniaczky <m.stepniak@yahoo.com>
2023-01-06 17:59:13 +0100

    Fixing env variables receiving methods

--- config/config.py
+++ config/config.py
@@ -2,16 +2,11 @@ import os
 
 from pymongo import MongoClient
 from fastapi import HTTPException
-from pathlib import Path
+
+from config.settings import CONFIG
 
 
 def config():
-    global CONFIG
-    CONFIG = {
-        'MONGODB_CONN_STRING': os.environ.get('MONGODB_CONN_STRING'),
-        'DB': os.environ.get('DB')
-    }
-
     if 'MONGODB_CONN_STRING' not in CONFIG or 'DB' not in CONFIG:
         raise HTTPException(
             status_code=500, detail='MongoDB connection string or database name not found in env file')
--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,16 @@
 import os
+from pathlib import Path
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 30  # 30 minutes
 REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7  # 7 days
 ALGORITHM = "HS256"
-JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
-JWT_REFRESH_SECRET_KEY = os.environ.get('JWT_REFRESH_SECRET_KEY')
+
+ENV_PATH = Path(__file__).parent.parent / '.env'
+
+CONFIG = {
+    'MONGODB_CONN_STRING': os.environ['MONGODB_CONN_STRING'],
+    'DB': os.environ['DB']
+}
+
+JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY']
+JWT_REFRESH_SECRET_KEY = os.environ['JWT_REFRESH_SECRET_KEY']
